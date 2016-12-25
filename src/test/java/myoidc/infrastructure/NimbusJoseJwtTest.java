f33e52b7d92996425bba437bb38ca0edf3cecb26
monkeyk7 <monkeyk1987@gmail.com>
2016-12-25 21:47:44 +0800

    JWT testing

--- src/test/java/myoidc/infrastructure/NimbusJoseJwtTest.java
+++ src/test/java/myoidc/infrastructure/NimbusJoseJwtTest.java
@@ -3,6 +3,8 @@ package myoidc.infrastructure;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.*;
 import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import net.minidev.json.JSONObject;
 import org.apache.commons.lang.RandomStringUtils;
 import org.testng.annotations.Test;
@@ -13,6 +15,7 @@ import java.security.PrivateKey;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -30,6 +33,7 @@ public class NimbusJoseJwtTest {
 
 
     /**
+     * JWS
      * 使用HMAC SHA-256 进行加密 与 解密
      * 基于相同的 secret (对称算法)
      * <p/>
@@ -69,8 +73,56 @@ public class NimbusJoseJwtTest {
         assertEquals(decryptPayload, payloadText);
     }
 
+    /**
+     * JWT
+     * 使用HMAC SHA-256 进行加密 与 解密
+     * 基于相同的 secret (对称算法)
+     * <p/>
+     * 算法     Secret长度
+     * HS256   32
+     * HS384   64
+     * HS512   64
+     *
+     * @throws Exception
+     */
+    @Test
+    public void jwtMAC() throws Exception {
+
+        String sharedSecret = RandomStringUtils.random(64, true, true);
+        JWSSigner jwsSigner = new MACSigner(sharedSecret);
+
+        //生成idToken
+        final String payloadText = "I am MyOIDC";
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("subject")
+                .issuer("https://andaily.com")
+                .claim("payloadText", payloadText)
+                .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+                .build();
+
+//        final JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+//        final JWSHeader header = new JWSHeader(JWSAlgorithm.HS384);
+        final JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+        signedJWT.sign(jwsSigner);
+
+        final String idToken = signedJWT.serialize();
+
+        //校验idToken
+        final SignedJWT parseJWT = SignedJWT.parse(idToken);
+        JWSVerifier jwsVerifier = new MACVerifier(sharedSecret);
+        final boolean verify = parseJWT.verify(jwsVerifier);
+
+        assertTrue(verify);
+//        final Payload payload = parseJWT.getPayload();
+        final JWTClaimsSet jwtClaimsSet = parseJWT.getJWTClaimsSet();
+        assertEquals(jwtClaimsSet.getSubject(), "subject");
+
+    }
+
 
     /**
+     * JWS
      * 使用 RSA 算法 生成 id_token
      * 以及对其进行校验(verify)
      * 需要公私钥对
@@ -131,12 +183,83 @@ public class NimbusJoseJwtTest {
         final boolean verify = parseJWS.verify(verifier);
         assertTrue(verify);
 
-        final Payload payload1 = jwsObject.getPayload();
+        final Payload payload1 = parseJWS.getPayload();
         assertNotNull(payload1);
         final JSONObject jsonObject1 = payload1.toJSONObject();
         assertNotNull(jsonObject1);
 
         assertEquals(payloadText, jsonObject1.get("payloadText"));
+
+    }
+
+
+    /**
+     * JWT
+     * 使用 RSA 算法 生成 id_token
+     * 以及对其进行校验(verify)
+     * 需要公私钥对
+     * <p/>
+     * 支持算法
+     * RS256
+     * RS384
+     * RS512
+     *
+     * @throws Exception
+     */
+    @Test
+    public void jwtRSA() throws Exception {
+
+        // RSA keyPair Generator
+        final KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        /**
+         * 长度 至少 1024, 建议 2048
+         */
+        final int keySize = 2048;
+        keyPairGenerator.initialize(keySize);
+
+        final KeyPair keyPair = keyPairGenerator.genKeyPair();
+        //公钥
+        final RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        //私钥
+        final PrivateKey privateKey = keyPair.getPrivate();
+
+        //keyId
+        String keyId = RandomUtils.randomNumber();
+
+        //生成id_token
+        JWSSigner jwsSigner = new RSASSASigner(privateKey);
+
+//        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(keyId).build();
+//        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS384).keyID(keyId).build();
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS512).keyID(keyId).build();
+
+        final String payloadText = "I am MyOIDC [RSA]";
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("subject")
+                .issuer("Issuer")
+                .audience("Audience")
+                .claim("payloadText", payloadText)
+                .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+
+        signedJWT.sign(jwsSigner);
+        final String idToken = signedJWT.serialize();
+        System.out.println(payloadText + " -> id_token: " + idToken);
+
+
+        //校验 id_token
+        final SignedJWT parseJWT = SignedJWT.parse(idToken);
+
+        JWSVerifier verifier = new RSASSAVerifier(publicKey);
+        final boolean verify = parseJWT.verify(verifier);
+        assertTrue(verify);
+
+        final JWTClaimsSet jwtClaimsSet = parseJWT.getJWTClaimsSet();
+        assertNotNull(jwtClaimsSet);
+        assertEquals(payloadText, jwtClaimsSet.getStringClaim("payloadText"));
+
 
     }
 
