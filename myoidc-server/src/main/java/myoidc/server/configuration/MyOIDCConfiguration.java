216c3b822c280c1acc8628bcccdd502e378d8248
monkeyk7 <zhao@idsmanager.com>
2018-03-22 16:32:44 +0800

    jpa config

--- myoidc-server/src/main/java/myoidc/server/configuration/MyOIDCConfiguration.java
+++ myoidc-server/src/main/java/myoidc/server/configuration/MyOIDCConfiguration.java
@@ -2,17 +2,10 @@ package myoidc.server.configuration;
 
 
 import myoidc.server.domain.shared.Application;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 
 /**
@@ -28,33 +21,6 @@ public class MyOIDCConfiguration {
 
     @Value("${application.host}")
     private String host;
-
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setPackagesToScan("myoidc.server.domain");
-
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-        properties.setProperty("hibernate.show_sql", "false");
-//        properties.setProperty("hibernate.hbm2ddl.auto", "none");
-        properties.setProperty("jdbc.use_scrollable_resultset", "false");
-        properties.setProperty("hibernate.query.substitutions", "true=1,false=0");
-        properties.setProperty("hibernate.current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
-        factoryBean.setHibernateProperties(properties);
-
-        return factoryBean;
-    }
-
-
-    @Bean
-    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
-    }
 
 
     // Application
