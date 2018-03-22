376dad6b4c177b56ad75d92e13292c6b6fc03b92
monkeyk7 <zhao@idsmanager.com>
2018-03-22 20:33:57 +0800

    ApplicationContext aware

---
+++ myoidc-server/src/main/java/myoidc/server/web/context/OIDCApplicationContextAware.java
@@ -0,0 +1,36 @@
+package myoidc.server.web.context;
+
+
+import myoidc.server.domain.shared.Application;
+import myoidc.server.domain.shared.BeanProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 2018/03/22
+ * <p>
+ *
+ * @author Shengzhao Li
+ */
+@Component
+public class OIDCApplicationContextAware implements ApplicationContextAware {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(OIDCApplicationContextAware.class);
+
+
+    public OIDCApplicationContextAware() {
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        BeanProvider.initialize(applicationContext);
+
+        LOG.info("Initialed BeanProvider from ApplicationContext: {}", applicationContext);
+        LOG.info("[oidc-server] context initialized, Version: {}\n", Application.VERSION);
+    }
+}
