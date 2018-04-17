49e1b0dee34ef251edfdf0b04be7d9b7c50b4fe0
monkeyk7 <zhao@idsmanager.com>
2018-04-17 22:09:59 +0800

    OAuth config

---
+++ myoidc-server/src/main/java/myoidc/server/configuration/OAuth2ServerConfiguration.java
@@ -0,0 +1,162 @@
+package myoidc.server.configuration;
+
+import myoidc.server.service.OauthService;
+import myoidc.server.service.SecurityService;
+import myoidc.server.service.oauth.CustomJdbcClientDetailsService;
+import myoidc.server.service.oauth.CustomJdbcTokenStore;
+import myoidc.server.service.oauth.OauthUserApprovalHandler;
+import myoidc.server.service.oauth.SOSAuthorizationCodeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.sql.DataSource;
+
+/**
+ * 2018/2/8
+ * <p>
+ * <p>
+ * OAuth2 config
+ *
+ * @author Shengzhao Li
+ */
+@Configuration
+public class OAuth2ServerConfiguration {
+
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            resources.resourceId("unity-resource").stateless(false)
+                    .resourceId("mobile-resource").stateless(false);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    // Since we want the protected resources to be accessible in the UI as well we need
+                    // session creation to be allowed (it's disabled by default in 2.0.6)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .and()
+                    .requestMatchers().antMatchers("/unity/**", "/m/**")
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/unity/**").access("#oauth2.hasScope('read') and hasRole('ROLE_UNITY')")
+                    .antMatchers("/m/**").access("#oauth2.hasScope('read') and hasRole('ROLE_MOBILE')");
+
+        }
+
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+        @Autowired
+        private TokenStore tokenStore;
+
+
+        @Autowired
+        private ClientDetailsService clientDetailsService;
+
+
+
+        @Autowired
+        private OauthService oauthService;
+
+
+        @Autowired
+        private AuthorizationCodeServices authorizationCodeServices;
+
+
+        @Autowired
+        private SecurityService userDetailsService;
+
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+            clients.withClientDetails(clientDetailsService);
+        }
+
+
+        @Bean
+        public TokenStore tokenStore(DataSource dataSource) {
+            return new CustomJdbcTokenStore(dataSource);
+        }
+
+
+        @Bean
+        public ClientDetailsService clientDetailsService(DataSource dataSource) {
+            return new CustomJdbcClientDetailsService(dataSource);
+        }
+
+
+        @Bean
+        public AuthorizationCodeServices authorizationCodeServices(DataSource dataSource) {
+            return new SOSAuthorizationCodeServices(dataSource);
+        }
+
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.tokenStore(tokenStore)
+                    .authorizationCodeServices(authorizationCodeServices)
+                    .userDetailsService(userDetailsService)
+                    .userApprovalHandler(userApprovalHandler())
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+//            oauthServer.authenticationEntryPoint()
+            oauthServer.realm("MyOIDC");
+        }
+
+        @Bean
+        public OAuth2RequestFactory oAuth2RequestFactory() {
+            return new DefaultOAuth2RequestFactory(clientDetailsService);
+        }
+
+
+        @Bean
+        public UserApprovalHandler userApprovalHandler() {
+            OauthUserApprovalHandler userApprovalHandler = new OauthUserApprovalHandler();
+            userApprovalHandler.setOauthService(oauthService);
+            userApprovalHandler.setTokenStore(tokenStore);
+            userApprovalHandler.setClientDetailsService(this.clientDetailsService);
+            userApprovalHandler.setRequestFactory(oAuth2RequestFactory());
+            return userApprovalHandler;
+        }
+
+    }
+
+
+}
--- myoidc-server/src/main/java/myoidc/server/configuration/SecurityConfiguration.java
+++ myoidc-server/src/main/java/myoidc/server/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 2018/2/1
@@ -33,6 +35,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityService securityService;
 
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -61,6 +69,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .usernameParameter("oidc_user")
                 .passwordParameter("oidcPwd")
                 .and()
+                .csrf()
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/token"))
+                .disable()
                 .logout()
                 .logoutUrl("/signout")
                 .deleteCookies("JSESSIONID")
