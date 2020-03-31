e651a7e5e0bf2c404ae8a95e28a13f1df7e305bf
monkeyk7 <monkeyk1987@gmail.com>
2020-03-31 23:26:06 +0800

    注册客户端API实现

---
+++ myoidc-server/src/main/java/myoidc/server/infrastructure/oidc/OIDCUseScene.java
@@ -0,0 +1,32 @@
+package myoidc.server.infrastructure.oidc;
+
+/**
+ * 2020/3/31
+ * <p>
+ * OIDC具体的使用场景
+ *
+ * @author Shengzhao Li
+ * @since 1.1.0
+ */
+public enum OIDCUseScene {
+
+    WEB(OIDCUtils.GrantType.AUTHORIZATION_CODE, OIDCUtils.GrantType.REFRESH_TOKEN),   //浏览器
+    CELL_PHONE(OIDCUtils.GrantType.PASSWORD, OIDCUtils.GrantType.REFRESH_TOKEN),   //移动设备(手机)
+    DEVICE_CLIENT(OIDCUtils.GrantType.PASSWORD, OIDCUtils.GrantType.REFRESH_TOKEN),  // 其他设备
+    SERVER(OIDCUtils.GrantType.CLIENT_CREDENTIALS);   //服务端
+
+
+    /**
+     * 每种场景对应的 grant_types
+     */
+    private OIDCUtils.GrantType[] grantTypes;
+
+    OIDCUseScene(OIDCUtils.GrantType... grantTypes) {
+        this.grantTypes = grantTypes;
+    }
+
+
+    public OIDCUtils.GrantType[] grantTypes() {
+        return this.grantTypes;
+    }
+}
--- myoidc-server/src/main/java/myoidc/server/web/controller/endpoint/RegistrationEndpoint.java
+++ myoidc-server/src/main/java/myoidc/server/web/controller/endpoint/RegistrationEndpoint.java
@@ -1,9 +1,11 @@
 package myoidc.server.web.controller.endpoint;
 
+import myoidc.server.infrastructure.oidc.OIDCUseScene;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 2020/3/11
@@ -21,7 +23,23 @@ public class RegistrationEndpoint {
     // 引导 注册
     @GetMapping("registration")
     public String preRegistration(Model model) throws Exception {
+        return "registration_pre";
+    }
+
+
+    /**
+     * 开始注册 form
+     *
+     * @param useScene 使用场景, 默认为 WEB
+     * @param model    Model
+     * @return view
+     * @throws Exception e
+     */
+    @GetMapping("registration_form")
+    public String registration(@RequestParam("scene") OIDCUseScene useScene, Model model) throws Exception {
 
         return "registration_form";
     }
+
+
 }
